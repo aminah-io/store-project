3d4d39a08dd339e387c38d71452eb134a0e1ced3
Aminah Burch <hello@aminah.io>
2021-03-09 16:10:43 +0300

    Initial commit

--- App.js
+++ App.js
@@ -1,14 +1,45 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import AppLoading from 'expo-app-loading';
+import { enableScreens } from 'react-native-screens';
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+import {
+  useFonts,
+  PlayfairDisplay_400Regular,
+  PlayfairDisplay_500Medium,
+  PlayfairDisplay_600SemiBold,
+  PlayfairDisplay_700Bold,
+} from '@expo-google-fonts/playfair-display';
+
+import ProductsNavigator from '../navigation/ProductsNavigator';
+
+//import the reducer file
+
+enableScreens();
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  let [fontsLoaded] = useFonts({
+    PlayfairDisplay_400Regular,
+    PlayfairDisplay_500Medium,
+    PlayfairDisplay_600SemiBold,
+    PlayfairDisplay_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return (
+      <AppLoading 
+        onError={(err) => console.log(err)}
+      />
+    );
+  } else {
+    return (
+      <Provider store={store}>
+        <ProductsNavigator />
+      </Provider>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
